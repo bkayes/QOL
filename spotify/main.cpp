2b74c48444bf7369b2d78fcfc130d3918ea00010
bkayes <brandonkayes@gmail.com>
2019-01-25 17:08:43 -0500

    Initial commit

---
+++ spotify/main.cpp
@@ -0,0 +1,6 @@
+
+//When this application is running, every _______ time (when spotify makes a new discover weekly)
+//it accesses all the new songs in this playlist, and adds them to an archive playlist
+int main(int argc, char **argv) {
+
+}
